2c76d75cf99e810fa39c467beed51fe329a5ed9c
Valentin Kozhevnik <valentinkozhevnik@gmail.com>
2014-10-23 08:14:39 +0300

    initial package and setup file

---
+++ setup.py
@@ -0,0 +1,17 @@
+__author__ = 'vako'
+
+from setuptools import setup
+
+setup(
+    name="django-psql-view",
+    url="https://github.com/valentinkozhevnik/django-psql-view",
+    author="valentin Kozhevnik",
+    author_email="valentinkozhevnik@gmail.com",
+    version="0.0.1a",
+    packages=[
+        'psqlview'
+    ],
+    install_requires=[
+        'django>=1.6'
+    ]
+)
